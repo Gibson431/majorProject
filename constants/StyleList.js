b0da1df56688b6153d04d74852349d3b2056c031
Tim <40378249+Xekso@users.noreply.github.com>
2018-12-09 17:04:19 +1100

    Add files via upload

---
+++ constants/StyleList.js
@@ -0,0 +1,12 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 15,
+      backgroundColor: '#fff',
+    },
+
+    header: {
+        flex: 1,
+        backgroundColor: '#21759B',
+    }
+  });
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? 'ios-home' : 'md-home'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Notifications',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-notifications' : 'md-notifications'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Calendar',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-calendar' : 'md-calendar'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  SettingsStack,
+  LinksStack,
+
+});
